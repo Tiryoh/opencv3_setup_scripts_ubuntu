f83b3433e6717cb5c90348ace4e93a526fe7d8cb
Tiryoh <tiryoh@gmail.com>
2017-08-07 18:43:15 +0900

    Add install script file

---
+++ opencv_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eu
+
+OPENCV_VER=3.2.0
+
+sudo apt update
+# compiler
+sudo apt install -y build-essential
+# required
+sudo apt install -y zip cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+# optional
+sudo apt install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+mkdir -p /tmp/opencv3_installer && cd /tmp/opencv3_installer
+wget https://github.com/opencv/opencv/archive/${OPENCV_VER}.zip -O opencv${OPENCV_VER}.zip
+unzip opencv${OPENCV_VER}.zip
+wget https://github.com/opencv/opencv_contrib/archive/${OPENCV_VER}.zip -O opencv_contrib${OPENCV_VER}.zip
+unzip opencv_contrib${OPENCV_VER}.zip
+cd /tmp/opencv3_installer/opencv-${OPENCV_VER} && mkdir build && cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${OPENCV_VER}/modules  -DWITH_TBB=ON -DWITH_V4L=ON -DWITH_OPENGL=ON ..
+make -j4
+sudo make install
+#echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf
+sudo ldconfig
+#export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+#mkdir -p ~/tmp && mv /tmp/opencv3_installer ~/tmp/
