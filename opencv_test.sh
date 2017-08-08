50479510e2d82cf3379d99be49a0e04bf7df7bfb
Tiryoh <tiryoh@gmail.com>
2017-08-09 01:26:17 +0900

    Use absolute path for OpenCV test

--- opencv_test.sh
+++ opencv_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
+DIR=$(cd $(dirname $0); pwd) && echo $DIR
 python -c 'import cv2;print(cv2.__version__)'
-[ -e ./test/opencv_cvt_img.out ] && rm ./test/opencv_cvt_img.out
-cd ./test && make && ./opencv_cvt_img.out && [ -e gray_image.jpg ] && echo 'OpenCV3 Installed'
+[ -e $DIR/test/opencv_cvt_img.out ] && rm $DIR/test/opencv_cvt_img.out
+cd $DIR/test && make && ./opencv_cvt_img.out && [ -e gray_image.jpg ] && echo 'OpenCV3 Installed'
