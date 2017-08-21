8eb08547a203f976aec30569423b81ffa459e975
Tiryoh <tiryoh@gmail.com>
2017-08-21 23:01:11 +0900

    Fix wget timeout problem

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
 OPENCV_VER=3.2.0
+WGET_TIMEOUT=60
 
 sudo apt-get update
 # compiler
@@ -10,9 +11,9 @@ sudo apt-get install -y zip cmake git libgtk2.0-dev pkg-config libavcodec-dev li
 # optional
 sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 mkdir -p /tmp/opencv3_installer && cd /tmp/opencv3_installer
-wget https://github.com/opencv/opencv/archive/${OPENCV_VER}.zip -O opencv${OPENCV_VER}.zip
+wget --timeout ${WGET_TIMEOUT} https://github.com/opencv/opencv/archive/${OPENCV_VER}.zip -O opencv${OPENCV_VER}.zip
 unzip opencv${OPENCV_VER}.zip
-wget https://github.com/opencv/opencv_contrib/archive/${OPENCV_VER}.zip -O opencv_contrib${OPENCV_VER}.zip
+wget --timeout ${WGET_TIMEOUT} https://github.com/opencv/opencv_contrib/archive/${OPENCV_VER}.zip -O opencv_contrib${OPENCV_VER}.zip
 unzip opencv_contrib${OPENCV_VER}.zip
 cd /tmp/opencv3_installer/opencv-${OPENCV_VER} && mkdir build && cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${OPENCV_VER}/modules  -DWITH_TBB=ON -DWITH_V4L=ON -DWITH_OPENGL=ON ..
